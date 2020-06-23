6a11dffaa837babc1ad53564b181cfae61b3c075
Jennifer Kruk <jenkruk@gmail.com>
2020-06-22 21:51:44 -0700

    fixing header image for gh pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from "./components/Container";
 import Wrapper from "./components/Wrapper";
 import ZooCard from "./components/ZooCard";
 import zoo from "./zoo.json";
+import zooHeader from "./zooHeader.jpg"
 
 class App extends Component {
   // Setting this.state.zoo to the friends json array
@@ -15,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <>
-      <Header backgroundImage="../../zooHeader.jpg"></Header>
+      <Header backgroundImage={zooHeader}></Header>
       <Container>
       <Wrapper>
         {this.state.zoo.map(zoo => (
