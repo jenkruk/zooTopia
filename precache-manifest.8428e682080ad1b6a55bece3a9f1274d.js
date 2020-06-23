fc08234e558103638b16fa2239607de921cbf59a
Jennifer Kruk <jenkruk@gmail.com>
2020-06-22 21:48:02 -0700

    Updates

---
+++ precache-manifest.8428e682080ad1b6a55bece3a9f1274d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da03cc642f952d14a9707d269f36830",
+    "url": "/zootopia/index.html"
+  },
+  {
+    "revision": "7ce1a0832e2832f9ed5a",
+    "url": "/zootopia/static/css/main.1e6198aa.chunk.css"
+  },
+  {
+    "revision": "c7a06f6ae06e22fc8e76",
+    "url": "/zootopia/static/js/2.5f183159.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zootopia/static/js/2.5f183159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ce1a0832e2832f9ed5a",
+    "url": "/zootopia/static/js/main.ad1eb5d1.chunk.js"
+  },
+  {
+    "revision": "358c68da353b0a17a99c",
+    "url": "/zootopia/static/js/runtime-main.a31d2ffb.js"
+  }
+]);
